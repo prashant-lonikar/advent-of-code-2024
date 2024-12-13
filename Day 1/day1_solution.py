f28c5846bb92e97c1f4309b4101e69fad861121f
Prashant Lonikar <prashant.lonikar@gmail.com>
2024-12-13 20:15:24 +0530

    Better file naming

--- Day 1/day1_solution.py
+++ Day 1/day1_solution.py
@@ -4,15 +4,10 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 # import a local txt file and print first few characters
-with open("input.txt", "r") as file:
+with open("day1_input.txt", "r") as file:
     data = file.read()
 
-row = 0
-switch = False
-list_1 = []
-list_2 = []
-temp_char = ""
-
+# Part 1
 list_1 = []
 list_2 = []
 for line in data.splitlines():
@@ -22,8 +17,16 @@ for line in data.splitlines():
 list_1 = sorted(list_1)
 list_2 = sorted(list_2)
 
-distances = 0
+distance = 0
 for i in range(len(list_1)):
-    distances += abs(list_1[i] - list_2[i])
+    distance += abs(list_1[i] - list_2[i])
 
-print(distances)
+print("Distance: ", distance)
+
+# Part 2
+similarity_score = 0
+for i in range(len(list_1)):
+    count_of_number_in_list_2 = list_2.count(list_1[i])
+    similarity_score += list_1[i] * count_of_number_in_list_2
+
+print("Similarity score: ", similarity_score)
